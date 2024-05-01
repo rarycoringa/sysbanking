bde77401da9ae30ff9491d3f5648cff02f615d58
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2024-05-01 17:41:47 -0300

    change verbose name to use in views

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Account(models.Model):
     id = models.UUIDField(
-        verbose_name="Identifier of Account",
+        verbose_name="Account Identifier",
         primary_key=True,
         unique=True,
         blank=False,
@@ -15,14 +15,14 @@ class Account(models.Model):
     )
 
     number = models.PositiveIntegerField(
-        verbose_name="Number of Account",
+        verbose_name="Account Number",
         unique=True,
         blank=False,
         null=False,
     )
 
     balance = models.DecimalField(
-        verbose_name="Balance of Account",
+        verbose_name="Account Balance",
         max_digits=15,
         decimal_places=2,
         blank=False,
