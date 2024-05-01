0f1f4952adf007f17f0d8f1bb6b508cf2e717354
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2024-05-01 16:49:09 -0300

    create Account model with required attributes

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,31 @@
+import uuid
+import decimal
+
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    id: uuid.UUID = models.UUIDField(
+        verbose_name="Identifier of Account",
+        primary_key=True,
+        unique=True,
+        blank=False,
+        null=False,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    number: int = models.PositiveIntegerField(
+        verbose_name="Number of Account",
+        unique=True,
+        blank=False,
+        null=False,
+    )
+
+    balance: decimal.Decimal = models.DecimalField(
+        verbose_name="Balance of Account",
+        max_digits=15,
+        decimal_places=2,
+        blank=False,
+        null=False,
+        default=decimal.Decimal(0.0),
+    )
