ce7c3b891aaedd70a70df7e245b34ac5869551b2
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2024-05-01 19:54:37 -0300

    implement view to create accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from accounts.models import Account
+
+class CreateAccountView(SuccessMessageMixin, CreateView):
+    fields = ["number"]
+    model = Account
+    success_url = reverse_lazy('accounts:create')
+    template_name = "accounts/create.html"
+
+    def get_success_message(self, cleaned_data: dict[str, str]) -> str:
+        account: Account = self.object
+        return f"Account Nº {account.number} was successfully created."
