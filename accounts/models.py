d44db08b77f8d2078b7b7675b8d7a5a9a5eaaf48
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2024-05-08 21:03:04 -0300

    update database and model with new account type

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import uuid
 import decimal
 
@@ -35,6 +37,14 @@ class Account(models.Model):
         validators=[MinValueValidator(decimal.Decimal(0.0))]
     )
 
+    @property
+    def type(self) -> str:     
+        return "simple"
+    
+    @property
+    def verbose_type(self) -> str:
+        return "Account"
+
     def deposit(self, amount: decimal.Decimal) -> None:
         if type(amount) is not decimal.Decimal:
             amount: decimal.Decimal = decimal.Decimal(amount)
@@ -42,25 +52,7 @@ class Account(models.Model):
         self.balance: decimal.Decimal = self.balance + amount
 
         self.save()
-    
-    def transfer(self, amount: decimal.Decimal, to_account: int) -> None:
-        if type(amount) is not decimal.Decimal:
-            amount: decimal.Decimal = decimal.Decimal(amount)
 
-        if self.balance < amount:
-            raise InsufficientBalance("Account doesn't have sufficient balance.")
-
-        if type(amount) is not int:
-            to_account: int = int(to_account)
-
-        to_account: Account = Account.objects.get(number=to_account)
-
-        self.withdraw(amount)
-        to_account.deposit(amount)
-        
-        self.save()
-        to_account.save()
-        
     def withdraw(self, amount: decimal.Decimal) -> None:
         if type(amount) is not decimal.Decimal:
             amount: decimal.Decimal = decimal.Decimal(amount)
@@ -68,6 +60,64 @@ class Account(models.Model):
         if self.balance < amount:
             raise InsufficientBalance("Account doesn't have sufficient balance.")
 
-        self.balance = self.balance - amount
+        self.balance: decimal.Decimal = self.balance - amount
 
         self.save()
+    
+    def transfer_deposit(self, amount: decimal.Decimal) -> None:
+        self.deposit(amount)
+
+    def transfer_withdraw(self, amount: decimal.Decimal) -> None:
+        self.withdraw(amount)
+
+    @staticmethod
+    def transfer(amount: decimal.Decimal, from_account: Account, to_account: Account) -> None:
+        if type(amount) is not decimal.Decimal:
+            amount: decimal.Decimal = decimal.Decimal(amount)
+
+        from_account.transfer_withdraw(amount)
+        to_account.transfer_deposit(amount)
+
+
+class BonusAccount(Account):
+    points = models.PositiveIntegerField(
+        verbose_name="Account Points",
+        blank=False,
+        null=False,
+        default=10,
+    )
+
+    @property
+    def type(self) -> str:     
+        return "bonus"
+
+    @property
+    def verbose_type(self) -> str:
+        return "Bonus Account"
+    
+    def add_points(self, amount: decimal.Decimal, cutoff_amount: decimal.Decimal) -> None:
+        if type(amount) is not decimal.Decimal:
+            amount: decimal.Decimal = decimal.Decimal(amount)
+
+        if type(cutoff_amount) is not decimal.Decimal:
+            cutoff_amount: decimal.Decimal = decimal.Decimal(cutoff_amount)
+
+        points: int = int(amount // cutoff_amount)
+
+        self.points: decimal.Decimal = self.points + points
+        
+        self.save()
+
+    def deposit(self, amount: decimal.Decimal, cutoff_amount: decimal.Decimal = decimal.Decimal(100.00)) -> None:
+        if type(amount) is not decimal.Decimal:
+            amount: decimal.Decimal = decimal.Decimal(amount)
+
+        self.balance: decimal.Decimal = self.balance + amount
+
+        self.add_points(amount, cutoff_amount)
+
+        self.save()
+
+    def transfer_deposit(self, amount: decimal.Decimal, cutoff_amount: decimal.Decimal = decimal.Decimal(200.00)) -> None:
+        self.deposit(amount, cutoff_amount)
+    
