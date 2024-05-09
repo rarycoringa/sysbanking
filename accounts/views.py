c9f6bd5bf8ca1c812c430d1ff88658b0bf8ad8f9
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2024-05-08 21:07:07 -0300

    update views with new logic to allow new kind of account

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from typing import Any
 from typing import Dict
 from typing import List
+from typing import Set
 from typing import Union
 from typing import Callable
 
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.exceptions import ValidationError
+from django.db.models.base import Model as Model
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
@@ -17,8 +19,11 @@ from django.views.generic import DetailView
 from django.views.generic import ListView
 
 from accounts.mixins import CurrentYearMixin
+from accounts.mixins import GetAccountMultipleTypesMixin
 from accounts.mixins import TemplateTitleMixin
+from accounts.mixins import TransactionValidationMixin
 from accounts.models import Account
+from accounts.models import BonusAccount
 
 
 class ListAccountView(CurrentYearMixin, TemplateTitleMixin, ListView):
@@ -27,8 +32,25 @@ class ListAccountView(CurrentYearMixin, TemplateTitleMixin, ListView):
     template_name: str = "accounts/list.html"
     template_title: str = "Account List"
 
+    def get_queryset(self) -> Set[Account | BonusAccount]:
+        simple_accounts: List[Account] = [
+            account for account in self.model.objects.filter(
+                bonusaccount__isnull=True,
+            )
+        ]
 
-class DetailAccountView(CurrentYearMixin, TemplateTitleMixin, DetailView):
+        bonus_accounts: List[BonusAccount] = [
+            account for account in BonusAccount.objects.all()
+        ]
+
+        all_accounts: Set[Account | BonusAccount] = {
+            account for account in simple_accounts + bonus_accounts
+        }
+
+        return all_accounts
+
+
+class DetailAccountView(CurrentYearMixin, GetAccountMultipleTypesMixin, TemplateTitleMixin, DetailView):
     context_object_name: str = "account"
     model: Account = Account
     slug_field: str = "number"
@@ -40,6 +62,7 @@ class DetailAccountView(CurrentYearMixin, TemplateTitleMixin, DetailView):
         context_data: Dict[str, Any] = super().get_context_data(**kwargs)
 
         retrieved_messages: List[Any] = messages.get_messages(self.request)
+
         if retrieved_messages:
             context_data["messages"] = retrieved_messages
 
@@ -52,32 +75,31 @@ class CreateAccountView(SuccessMessageMixin, CurrentYearMixin, TemplateTitleMixi
     success_url = reverse_lazy('accounts:create')
     template_name: str = "accounts/create.html"
     template_title: str = "Create Accounts"
+    model_class_map: Dict[str, Account] = {
+        "simple": Account,
+        "bonus": BonusAccount,
+    }
 
     def get_success_message(self, cleaned_data: Dict[str, str]) -> str:
         account: Account = self.object
         return f"Account Nº {account.number} was successfully created."
+    
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        type: str = request.POST.get("type")
+
+        self.model: Account | BonusAccount = self.model_class_map[type]
+
+        return super().post(request, *args, **kwargs)
 
 
-class TransactionView(DetailView):
-    transaction_name: str = None
-    transaction_parameters_names: List[str] = None
-
-    def transaction_valid(self, message: str = "Transaction sucessfully completed.") -> HttpResponseRedirect:
-        """If the form is valid, redirect to the supplied URL."""
-        messages.success(self.request, message, extra_tags="success")
-
-        return HttpResponseRedirect(self.get_success_url())
-
-    def transaction_invalid(self, reason: str = "Failed trying to do transaction.") -> HttpResponse:
-        """If the form is invalid, render the invalid form."""
-        messages.error(self.request, reason, extra_tags="danger")
-
-        return self.render_to_response(self.get_context_data())
+class TransactionView(GetAccountMultipleTypesMixin, TransactionValidationMixin, DetailView):
+    transaction_name: str
+    transaction_parameters_names: List[str]
 
     def post(self, request: HttpRequest, *args, **kwargs) -> Union[HttpResponse, HttpResponseRedirect]:
-        self.object: Account = self.get_object()
+        self.object: Account | BonusAccount = self.get_object()
 
-        transaction_parameters: Dict[str, str] = {
+        transaction_parameters: Dict[str, Any] = {
             param: request.POST[param]
             for param in self.transaction_parameters_names
         }
@@ -108,20 +130,6 @@ class MakeDepositView(TemplateTitleMixin, CurrentYearMixin, TransactionView):
         return reverse_lazy('accounts:detail', kwargs={"number": self.object.number})
 
 
-class MakeTransferView(TemplateTitleMixin, CurrentYearMixin, TransactionView):
-    context_object_name: str = "account"
-    model: Account = Account
-    slug_field: str = "number"
-    slug_url_kwarg: str = "number"
-    template_name: str = "accounts/transfer.html"
-    template_title: str = "Transfer"
-    transaction_name: str  = "transfer"
-    transaction_parameters_names: List[str] = ["amount", "to_account"]
-
-    def get_success_url(self) -> str:
-        return reverse_lazy('accounts:detail', kwargs={"number": self.object.number})
-
-
 class MakeWithdrawView(TemplateTitleMixin, CurrentYearMixin, TransactionView):
     context_object_name: str = "account"
     model: Account = Account
@@ -131,6 +139,53 @@ class MakeWithdrawView(TemplateTitleMixin, CurrentYearMixin, TransactionView):
     template_title: str = "Make Withdraw"
     transaction_name: str  = "withdraw"
     transaction_parameters_names: List[str] = ["amount"]
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('accounts:detail', kwargs={"number": self.object.number})
+    
+
+class TransferView(GetAccountMultipleTypesMixin, TransactionValidationMixin, DetailView):
+    transaction_name: str
+    transaction_parameters_names: List[str]
+    model: Account
+
+    def post(self, request: HttpRequest, *args, **kwargs) -> Union[HttpResponse, HttpResponseRedirect]:
+        self.object: Account | BonusAccount = self.get_object()
+
+        transaction_parameters: Dict[str, Any] = {
+            param: request.POST[param]
+            for param in self.transaction_parameters_names
+        }
+        
+        from_account: Account | BonusAccount = self.object
+        transaction_parameters["from_account"] = from_account
+
+        to_account: Account | BonusAccount = self.get_account_by_number(
+            transaction_parameters["to_account"],
+        )
+        transaction_parameters["to_account"] = to_account
+
+        transaction_method: Callable = self.model.transfer
+
+        try:
+            transaction_method(**transaction_parameters)
+        except ValidationError as err:
+            return self.transaction_invalid(err.message)
+
+        success_message: str = f"{self.transaction_name.title()} successfuly completed."
+
+        return self.transaction_valid(success_message)
+
+
+class MakeTransferView(TemplateTitleMixin, CurrentYearMixin, TransferView):
+    context_object_name: str = "account"
+    model: Account = Account
+    slug_field: str = "number"
+    slug_url_kwarg: str = "number"
+    template_name: str = "accounts/transfer.html"
+    template_title: str = "Transfer"
+    transaction_name: str  = "transfer"
+    transaction_parameters_names: List[str] = ["amount", "to_account"]
 
     def get_success_url(self) -> str:
         return reverse_lazy('accounts:detail', kwargs={"number": self.object.number})
