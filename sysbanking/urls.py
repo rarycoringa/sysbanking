f7dacf068ab3ce47827eff10fe2b57e0a6ff50e5
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2024-07-21 15:33:52 -0300

    Feature: Refactor service architecture to REST API with a frontend layer (#42)
    * add rest framework dependency
    
    * create api module and first routers
    
    * improve accounts module and create AccountType Enum
    
    * create routers to deposit and withdraw
    
    * create router to make transfers

--- sysbanking/urls.py
+++ sysbanking/urls.py
@@ -20,5 +20,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
+    path('api/', include('restapi.urls', 'api')),
 ]
